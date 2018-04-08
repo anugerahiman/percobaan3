88943d3b349a4832d764224fea5c9910006dbef8
anugerah noor iman <anugerahnooriman71@gmail.com>
2018-04-08 12:46:58 +0700

    buat method throttle()

--- kendaraan.java
+++ kendaraan.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package percobaan3;
 
-/**
- *
- * @author user
- */
 public class kendaraan {
-    
+    private String jenisKendaraan;
+
+    void throttle(){
+        System.out.println("Toyota Indonesia");
 }
