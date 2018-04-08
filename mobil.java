c9c3c5162b8067c8ca7c87118691977e7b9a0854
anugerah noor iman <anugerahnooriman71@gmail.com>
2018-04-08 12:34:04 +0700

    memasukan atribut dan panggil cetak info

--- mobil.java
+++ mobil.java
@@ -4,4 +4,11 @@ public class mobil {
   private String merk;
   private String warna;
   private double harga;
-}
+  
+  void cetakInfo(){
+       System.out.println("Merk \t:    "+merk+"\n"+
+                           "Warna \t:   "+warna+"\n"+
+                           "Harga  \t:"+harga);
+  
+  }
+  
