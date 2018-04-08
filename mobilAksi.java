1b6bd0aa3298b1f94dd97f67128332c9d87b96ea
anugerah noor iman <anugerahnooriman71@gmail.com>
2018-04-08 12:42:08 +0700

    memasukukan atribut dan method, panggil cetak info

--- mobilAksi.java
+++ mobilAksi.java
@@ -3,4 +3,17 @@ package percobaan3;
 public class mobilAksi {
    public static void main(String[] args){
         mobil A = new mobil();  
-}
+   
+        A.setMerk("Brio");
+        A.setWarna("Merah");
+        A.setHarga(170000000);
+        
+        A.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(A.getmerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(A.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(A.getHarga());
+   }
